039c1f0daef757ee8469d748f9691f56a70142e5
RoyLING <royling0024@gmail.com>
2015-07-24 12:10:43 +0800

    init commit

---
+++ .bashrc
@@ -0,0 +1,4 @@
+export PS1="\[\e[0;33m\][\u@\h]\[\e[m\] \[\e[1;33m\]\w\[\e[m\]\n$ "
+
+export CLICOLOR=1
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
---
+++ vim-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [[ -e "~/.vimrc" ]]; then
+    # backup .vimrc if it exists
+    mv ~/.vimrc ~/.vimrc.orig
+fi
+# link to .vimrc in this dir
+ln -s .vimrc ~/.vimrc
+
+# install pathogen
+if [[ ! -d "~/.vim" || ! -d "~/.vim/autoload" ]]; then
+    mkdir -p ~/.vim/autoload ~/.vim/bundle
+fi
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# install plugins
+cd ~/.vim/bundle
+git clone https://github.com/altercation/vim-colors-solarized
+#git clone https://github.com/chriskempson/base16-vim
+git clone https://github.com/kien/ctrlp.vim
+#git clone https://github.com/scrooloose/nerdtree
+#git clone https://github.com/bling/vim-airline
+git clone https://github.com/tpope/vim-surround
+#git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/airblade/vim-gitgutter
+git clone https://github.com/terryma/vim-multiple-cursors
+
+# anything else?
