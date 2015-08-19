edb625d1a9e30a3b1523b49f2dde901f0710430d
RoyLING <royling0024@gmail.com>
2015-08-19 21:22:33 +0800

    fixed ln

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ln .zshrc ~/.zshrc
+ln $PWD/.zshrc ~/.zshrc
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh && \
-    ln ys.zsh-theme ~/.oh-my-zsh/themes/ys-tweaked.zsh-theme
+    ln $PWD/ys.zsh-theme ~/.oh-my-zsh/themes/ys-tweaked.zsh-theme
 
