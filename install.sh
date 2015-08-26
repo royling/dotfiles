24c294c312e2240e421383a5d3827f682bd1348f
RoyLING <royling0024@gmail.com>
2015-08-26 20:50:15 +0800

    single install script

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
 abs_path=`cd $(readlink "$0" | xargs dirname); pwd -P`
 
+# backup .vimrc if it exists
 if [[ -e "~/.vimrc" ]]; then
-    # backup .vimrc if it exists
     mv ~/.vimrc ~/.vimrc.orig
 fi
-# link to .vimrc in this dir
 ln -s $abs_path/.vimrc ~/.vimrc
 
 # install pathogen
@@ -15,16 +14,23 @@ if [[ ! -d "~/.vim" || ! -d "~/.vim/autoload" ]]; then
 fi
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# install plugins
+# install vim plugins
 cd ~/.vim/bundle
 #git clone https://github.com/altercation/vim-colors-solarized
 git clone https://github.com/chriskempson/base16-vim
 git clone https://github.com/kien/ctrlp.vim
-#git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/bling/vim-airline
 git clone https://github.com/tpope/vim-surround
-#git clone https://github.com/scrooloose/syntastic
 git clone https://github.com/airblade/vim-gitgutter
 git clone https://github.com/terryma/vim-multiple-cursors
+
+if [[ -e "~/.zshrc" ]]; then
+    mv ~/.zshrc ~/.zshrc.orig
+fi
+ln $abs_path/.zshrc ~/.zshrc
+# install oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh && \
+ln $abs_path/ys.zsh-theme ~/.oh-my-zsh/themes/ys-tweaked.zsh-theme
 
 # anything else?
--- zsh-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-abs_path=`cd $(readlink "$0" | xargs dirname); pwd -P`
-ln $abs_path/.zshrc ~/.zshrc
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh && \
-    ln $abs_path/ys.zsh-theme ~/.oh-my-zsh/themes/ys-tweaked.zsh-theme
-
