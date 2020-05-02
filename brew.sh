1a8a48bf4441047f244078145251a38071b2087f
Roy Ling <royling0024@gmail.com>
2020-05-02 22:23:49 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,2 +1,7 @@
 brew install vim --with-override-system-vi
 brew install tree lftp trash wget dos2unix
+brew install tmux
+
+# Fira Code
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
